35873e666fd5b0095fb03c685d734d102a47e644
mchaikov <m.chaikov@artsofte.ru>
2020-12-07 19:14:25 +0300

    9

--- client/index.mjs
+++ client/index.mjs
@@ -3,22 +3,38 @@ import drawer from "./drawer.mjs";
 import picker from "./picker.mjs";
 
 document.querySelector("#start").addEventListener("submit", e => {
-  e.preventDefault();
-  main(new FormData(e.currentTarget).get("apiKey"));
-  document.querySelector(".container").classList.add("ready");
+    e.preventDefault();
+    main(new FormData(e.currentTarget).get("apiKey"));
+    document.querySelector(".container").classList.add("ready");
 });
 
 const main = apiKey => {
-  const ws = connect(apiKey);
-  ws.addEventListener("message", console.log);
+    const ws = connect(apiKey);
+    ws.addEventListener("message", (x) => {
+        let data = JSON.parse(x.data);
+        let dataObj = data.payload
+        if (data.type == 'sendField') {
+            console.log('sendField')
+            timeout.next = new Date();
+            drawer.putArray(dataObj)
 
-  timeout.next = new Date();
-  drawer.onClick = (x, y) => {
-    drawer.put(x, y, picker.color);
-  };
+            drawer.onClick = (x, y) => {
+                ws.send(JSON.stringify({
+                    type: "drawerClick",
+                    payload: { x: x, y: y, color: picker.color }
+                }))
+            };
+        }
+
+        if (data.type == 'drawerClick') {
+            drawer.put(dataObj.x, dataObj.y, dataObj.color);
+        }
+    });
+
+
 };
 
 const connect = apiKey => {
-  const url = `${location.origin.replace(/^http/, "ws")}?apiKey=${apiKey}`;
-  return new WebSocket(url);
+    const url = `${location.origin.replace(/^http/, "ws")}?apiKey=${apiKey}`;
+    return new WebSocket(url);
 };
--- server.mjs
+++ server.mjs
@@ -5,58 +5,101 @@ import WebSocket from "ws";
 const port = process.env.PORT || 5000;
 
 const apiKeys = new Set([
-  "4a83051d-aad4-483e-8fc8-693273d15dc7",
-  "c08c9038-693d-4669-98cd-9f0dd5ef06bf",
-  "4b1545c4-4a70-4727-9ea1-152ed4c84ae2",
-  "4a226908-aa3e-4a34-a57d-1f3d1f6cba84",
+    "4a83051d-aad4-483e-8fc8-693273d15dc7",
+    "c08c9038-693d-4669-98cd-9f0dd5ef06bf",
+    "4b1545c4-4a70-4727-9ea1-152ed4c84ae2",
+    "4a226908-aa3e-4a34-a57d-1f3d1f6cba84",
 ]);
 
 const colors = [
-  "#140c1c",
-  "#442434",
-  "#30346d",
-  "#4e4a4e",
-  "#854c30",
-  "#346524",
-  "#d04648",
-  "#757161",
-  "#597dce",
-  "#d27d2c",
-  "#8595a1",
-  "#6daa2c",
-  "#d2aa99",
-  "#6dc2ca",
-  "#dad45e",
-  "#deeed6",
+    "#140c1c",
+    "#442434",
+    "#30346d",
+    "#4e4a4e",
+    "#854c30",
+    "#346524",
+    "#d04648",
+    "#757161",
+    "#597dce",
+    "#d27d2c",
+    "#8595a1",
+    "#6daa2c",
+    "#d2aa99",
+    "#6dc2ca",
+    "#dad45e",
+    "#deeed6",
 ];
 
 const size = 256;
 // place(x, y) := place[x + y * size]
 const place = Array(size * size).fill(null);
 for (const [colorIndex, colorValue] of colors.entries()) {
-  for (let dx = 0; dx < size; dx++) {
-    place[dx + colorIndex * size] = colorValue;
-  }
+    for (let dx = 0; dx < size; dx++) {
+        place[dx + colorIndex * size] = colorValue;
+    }
 }
 
 const app = express();
 
 app.use(express.static(path.join(process.cwd(), "client")));
 
+app.get('/colors', async(req, res) => {
+    res.send(colors);
+
+})
+
 app.get("/*", (_, res) => {
-  res.send("Place(holder)");
+    res.send("Place(holder)");
 });
+
+
 
 const server = app.listen(port);
 
-const wss = new WebSocket.Server({
-  noServer: true,
-});
+const wss = new WebSocket.Server({ noServer: true });
+
+
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        console.log(message)
+        let data = JSON.parse(message);
+        let dataObj = data.payload;
+        if (data.type == 'drawerClick') {
+            if (dataObj.x < 0 && dataObj.y < 0 && dataObj.x > size && dataObj.y > size)
+                return new Error('invalid cordinates');
+
+            wss.clients.forEach((client) => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(message);
+                    console.log('sended');
+                }
+            });
+
+            place[dataObj.x + dataObj.y * size] = dataObj.color;
+        }
+    });
+
+    ws.send(JSON.stringify({
+        type: "sendField",
+        payload: place
+    }));
+})
 
 server.on("upgrade", (req, socket, head) => {
-  const url = new URL(req.url, req.headers.origin);
-  console.log(url);
-  wss.handleUpgrade(req, socket, head, (ws) => {
-    wss.emit("connection", ws, req);
-  });
+
+
+});
+
+server.on('upgrade', (req, socket, head) => {
+    let url = new URL(req.url, peq.headers.origin);
+    let key = url.searchParams.get('apiKey');
+    console.log(url);
+    if (!apiKeys.has(key))
+        socket.destroy();
+
+    wss.handleUpgrade(req, socket, head, (ws) => {
+
+        wss.emit("connection", ws, req);
+    });
 });
