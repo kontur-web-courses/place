ebe740880d5b5966bbbd10098217a803cc22acb8
Ilya <zdravulin@gmail.com>
2022-05-30 18:11:58 +0500

    Ведьмак 4

--- client/index.mjs
+++ client/index.mjs
@@ -3,22 +3,41 @@ import drawer from "./drawer.mjs";
 import picker from "./picker.mjs";
 
 document.querySelector("#start").addEventListener("submit", e => {
-  e.preventDefault();
-  main(new FormData(e.currentTarget).get("apiKey"));
-  document.querySelector(".container").classList.add("ready");
+    e.preventDefault();
+    main(new FormData(e.currentTarget).get("apiKey"));
+    document.querySelector(".container").classList.add("ready");
 });
 
 const main = apiKey => {
-  const ws = connect(apiKey);
-  ws.addEventListener("message", console.log);
+    const ws = connect(apiKey);
+    ws.addEventListener("message", e => {
+        const message = JSON.parse(e.data);
+        const payload = message.payload;
 
-  timeout.next = new Date();
-  drawer.onClick = (x, y) => {
-    drawer.put(x, y, picker.color);
-  };
+        switch (message.type) {
+            case 'place':
+                drawer.putArray(message.payload.place);
+                break;
+            case 'single-pixel':
+                drawer.put(payload.x, payload.y, payload.color);
+                break;
+        }
+    });
+
+    timeout.next = new Date();
+    drawer.onClick = (x, y) => {
+        ws.send(JSON.stringify({
+            type: "single-pixel",
+            payload: {
+                x: x,
+                y: y,
+                color: picker.color
+            }
+        }));
+    };
 };
 
 const connect = apiKey => {
-  const url = `${location.origin.replace(/^http/, "ws")}?apiKey=${apiKey}`;
-  return new WebSocket(url);
+    const url = `${location.origin.replace(/^http/, "ws")}?apiKey=${apiKey}`;
+    return new WebSocket(url);
 };
